25f2d845b93ebfb3a7b18d64a3dd3d2b1f95e5d9
MitchelBroekate <mitchelb11@outlook.com>
2023-09-21 11:43:33 +0200

    enemy script added + material

---
+++ W.I.P/Assets/Mitchel's stuff/Scripts/EnemyAI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+
+
+
+
+}
