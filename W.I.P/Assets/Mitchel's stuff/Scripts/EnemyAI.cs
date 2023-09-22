b7c1090d1fbce8fbc47565726518a4030968bf03
MitchelBroekate <mitchelb11@outlook.com>
2023-09-22 13:22:17 +0200

    Enemy Pathfinding
    enemy pathfinding complete and customizable

--- W.I.P/Assets/Mitchel's stuff/Scripts/EnemyAI.cs
+++ W.I.P/Assets/Mitchel's stuff/Scripts/EnemyAI.cs
@@ -5,11 +5,35 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    [SerializeField]
+    #region Links
     private NavMeshAgent agent;
 
+    [SerializeField]
+    private Transform[] checkpoints;
 
+    [SerializeField]
+    private int currentDest = 0;
+    #endregion
 
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+    }
 
+    private void Update()
+    {
+        NextPoint();
+    }
 
+    private void NextPoint()
+    {
+        agent.destination = checkpoints[currentDest].position;
+
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+                currentDest += 1;
+        }
+
+    }
 }
