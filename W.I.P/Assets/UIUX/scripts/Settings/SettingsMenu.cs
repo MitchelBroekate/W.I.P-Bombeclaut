9a33069a3a88bcf2e99a2a79667d2f515cf9fd97
MitchelBroekate <mitchelb11@outlook.com>
2023-09-29 11:17:18 +0200

    Settings res dropdown fix
    dropdown only uses one hertz

--- W.I.P/Assets/UIUX/scripts/Settings/SettingsMenu.cs
+++ W.I.P/Assets/UIUX/scripts/Settings/SettingsMenu.cs
@@ -1,35 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    #region Links
     public AudioMixer audioMaster;
 
     public TMP_Dropdown resDropDown;
+    public Resolution[] resolutions;
+    public List<Resolution> filteredRes;
+
+    public float currentRes;
+    public int currentResIndex = 0;
 
     Resolution[] res;
+    #endregion
 
+    #region Start
     private void Start()
     {
-        res = Screen.resolutions;
-
-        resDropDown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        for (int i = 0; i < res.Length; i++) 
-        {
-            string option = res[i].width + "x" + res[i].height;
-
-            options.Add(option);
-        }
-
-        resDropDown.AddOptions(options);
+        Res();
     }
+    #endregion
+
+    #region Sliders
     public void SFXSlider(float volumeS)
     {
         audioMaster.SetFloat("volumeSFX", volumeS);
@@ -39,9 +38,50 @@ public class SettingsMenu : MonoBehaviour
     {
         audioMaster.SetFloat("volumeMusic", volumeM);
     }
+    #endregion
 
+    #region Fullscreen
     public void ScreenFull(bool isFull)
     {
         Screen.fullScreen = isFull;
     }
+    #endregion
+
+    #region Resolution
+    public void Res()
+    {
+        res = Screen.resolutions;
+        filteredRes = new List<Resolution>();
+
+        resDropDown.ClearOptions();
+        currentRes = Screen.currentResolution.refreshRate;
+
+
+        for (int i = 0; i < res.Length; i++)
+        {
+            filteredRes.Add(res[i]);
+        }
+
+        List<string> options = new List<string>();
+        
+        for (int i = 0;i < filteredRes.Count;i++)
+        {
+            string resolutionOption = filteredRes[i].width + "x" + filteredRes[i].height;
+            options.Add(resolutionOption);
+            if (filteredRes[i].width == Screen.width && filteredRes[i].height == Screen.height)
+            {
+                currentRes = i;
+            }
+        }
+        resDropDown.AddOptions(options);
+        resDropDown.value = currentResIndex;
+        resDropDown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = filteredRes[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, true);
+    }
+    #endregion
 }
