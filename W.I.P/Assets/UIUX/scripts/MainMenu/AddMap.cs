17eaaafced046db948b6295a80abda985147d6d8
R4HK00N <robert.eddy.beltman@gmail.com>
2023-10-09 15:21:15 +0200

    main menu fix

---
+++ W.I.P/Assets/UIUX/scripts/MainMenu/AddMap.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AddMap : MonoBehaviour
+{
+    public void Awake()
+    {
+        SceneManager.LoadScene(4, LoadSceneMode.Additive);
+    }
+}
