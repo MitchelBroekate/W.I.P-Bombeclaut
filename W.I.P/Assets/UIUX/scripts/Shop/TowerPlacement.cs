c028b36381066e29066995d81ab346ad1266161d
MitchelBroekate <mitchelb11@outlook.com>
2023-10-18 16:17:14 +0200

    tower placement + in eigen scene

--- W.I.P/Assets/UIUX/scripts/Shop/TowerPlacement.cs
+++ W.I.P/Assets/UIUX/scripts/Shop/TowerPlacement.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TowerPlacement : MonoBehaviour
 {
 
-    public bool opDuracell = false, opMouseTrap = false, opSpray = false;
+    public bool opDuracell, opMouseTrap, opSpray, opHenry;
+
+    InputMaster controls;
+
+    private Vector3 mousePos;
+
+    private void Awake()
+    {
+        controls = new InputMaster();
+    }
 
     private void Update()
     {
@@ -12,17 +22,33 @@ public class TowerPlacement : MonoBehaviour
 
     public void DuracellButton()
     {
-        opDuracell = !opDuracell;   
+        opDuracell = !opDuracell;
+        opMouseTrap = false;
+        opSpray = false;
+        opHenry = false;
     }
 
     public void MouseTrapButton()
     {
         opMouseTrap = !opMouseTrap;
+        opSpray = false;
+        opHenry = false;
+        opDuracell = false;
     }
 
     public void SprayButton()
     {
         opSpray = !opSpray;
+        opHenry = false;
+        opDuracell = false;
+        opMouseTrap = false;
+    }
+    public void HenryButton()
+    {
+        opHenry = !opHenry;
+        opDuracell = false;
+        opMouseTrap = false;
+        opSpray = false;
     }
 
     private void SelectionVisual()
@@ -35,16 +61,28 @@ public class TowerPlacement : MonoBehaviour
         if (opDuracell)
         {
             Debug.Log("summin happen");
+            mousePos = controls.Player.Cam.ReadValue<Vector2>();
         }
 
         if (opMouseTrap)
         {
             Debug.Log("summin happen");
+            mousePos = controls.Player.Cam.ReadValue<Vector2>();
+
         }
 
         if (opSpray)
         {
             Debug.Log("summin happen");
+            mousePos = controls.Player.Cam.ReadValue<Vector2>();
+
+        }
+
+        if (opHenry)
+        {
+            Debug.Log("summin happen");
+            mousePos = controls.Player.Cam.ReadValue<Vector2>();
+
         }
     }
 }
