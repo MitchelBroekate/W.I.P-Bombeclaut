08dc15631bf3ca47be6a8ba7f22ff9adb980e3b9
MitchelBroekate <mitchelb11@outlook.com>
2023-11-03 10:40:35 +0100

    starting tower ai

---
+++ W.I.P/Assets/Mitchel's stuff/Scripts/Towers/TowerAI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAI : MonoBehaviour
+{
+    private void Update()
+    {
+        Duracell();
+        MouseTrap();
+        Spray();
+        Henry();
+    }
+    private void Duracell()
+    {
+
+    }
+
+    private void MouseTrap()
+    {
+
+    }
+
+    private void Spray()
+    {
+
+    }
+
+    private void Henry()
+    {
+
+    }
+}
