f4bbf6052baa19003e07986c2ecdecded3f5be94
R4HK00N <robert.eddy.beltman@gmail.com>
2023-11-09 11:25:01 +0100

    UI upgrades

--- W.I.P/Assets/UIUX/scripts/Shop/OnHoverShowTower.cs
+++ W.I.P/Assets/UIUX/scripts/Shop/OnHoverShowTower.cs
@@ -8,16 +8,20 @@ public class OnHoverShowTower : MonoBehaviour,IPointerEnterHandler,IPointerExitH
 {
     public TMP_Text towerNameShower;
     public TMP_Text towerCostShower;
+    public TMP_Text towerDescription;
     public string cost;
     public string towerName;
+    public string description;
     public void OnPointerEnter(PointerEventData eventData)
     {
         towerNameShower.text = towerName;
         towerCostShower.text = cost;
+        towerDescription.text = description;
     }
     public void OnPointerExit(PointerEventData eventData)
     {
         towerNameShower.text = "Choose a tower";
         towerCostShower.text = "0";
+        towerDescription.text = "Hover over a tower to get description";
     }
 }
